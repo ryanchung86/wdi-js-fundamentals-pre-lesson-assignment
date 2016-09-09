743429d9a2ddd8517595750421fa8548f71b26fd
Ryan <ryan.y.chung@gmail.com>
2016-09-08 21:54:55 -0400

    completed pre-lesson

---
+++ pacman-switch.js
@@ -0,0 +1,16 @@
+var ghost = 'Blinky'
+switch (ghost) {
+  case 'Inky':
+    colour = 'Cyan'
+    break
+  case 'Blinky':
+    colour = 'Red'
+    break
+  case 'Pinky':
+    colour = 'Pink'
+    break
+  case 'Clyde':
+    colour = 'Orange'
+    break
+}
+console.log(ghost + ' is the colour ' + colour)
